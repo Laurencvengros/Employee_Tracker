8e4cc4df0b65f4dcc2f829fb52f5a8d3be0f19c3
Lauren <l.cvengros@icloud.com>
2022-10-05 14:31:43 -0400

    added first prompt question

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
+const { exit } = require('process');
 
 
 
@@ -15,10 +16,60 @@ var db = mysql.createConnection(
 
 db.connect((err) => {
     if (err) {
-        throw err;
+        console.log("Error connecting");
     }else{
-        console.log("connected!")
+        console.log("Connected! Welcome to the Employee Manager!");
+        promptUser();
     }
   
 });
+
+
  
+function promptUser(){
+    
+    inquirer.prompt([
+        { 
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Exit application"
+            ]
+        }
+    ]).then(function(choice){
+        switch(choice.action){
+            case "View all departments":
+                viewDepartment();
+                break;
+            case "View all roles":
+                viewRoles();
+                break;
+            case "View all employees":
+                viewEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update an employee role":
+                updateRole();
+                break;
+            case "Exit application":
+                exit();
+
+        }
+    })
+}
